3fa4f881a34a133f3b386ee856b3bd81d408df6a
Dieguidev <diegogaraycullas@gmail.com>
2023-08-04 12:14:05 -0500

    Se crearon crud para producto

--- src/main/java/com/dieguidev/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/dieguidev/market/persistence/crud/ProductoCrudRepository.java
@@ -1,4 +1,16 @@
 package com.dieguidev.market.persistence.crud;
 
-public class ProductoCrudRepository {
+import com.dieguidev.market.persistence.entity.Producto;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    //@Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true)
+    //la linea de arriba es lo mismo que la linea de abajo solo que con querymethods
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
+
 }
