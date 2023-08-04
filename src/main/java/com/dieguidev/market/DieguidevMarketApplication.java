4d913508c293962278574e6d2ba395855bf81d95
Dieguidev <diegogaraycullas@gmail.com>
2023-08-03 23:20:42 -0500

    first commit

---
+++ src/main/java/com/dieguidev/market/DieguidevMarketApplication.java
@@ -0,0 +1,13 @@
+package com.dieguidev.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DieguidevMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DieguidevMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dieguidev/market/DieguidevMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dieguidev.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DieguidevMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
