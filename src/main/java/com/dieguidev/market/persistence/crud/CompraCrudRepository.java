89694a398225efebf7f32bc7968bafdc2cd7455c
Dieguidev <diegogaraycullas@gmail.com>
2023-08-06 09:05:06 -0500

    Creando repositorio compras

---
+++ src/main/java/com/dieguidev/market/persistence/crud/CompraCrudRepository.java
@@ -0,0 +1,12 @@
+package com.dieguidev.market.persistence.crud;
+
+import com.dieguidev.market.persistence.entity.Compra;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CompraCrudRepository extends CrudRepository<Compra, Integer> {
+    Optional<List<Compra>> findByIdCliente(String idCliente);
+
+}
